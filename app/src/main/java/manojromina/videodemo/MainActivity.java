18794118759ab5d9eb02b9f2951335a94f0dc279
Manoj <manojromina1997@gmail.com>
2017-05-05 00:26:20 +0530

    first commit

---
+++ app/src/main/java/manojromina/videodemo/MainActivity.java
@@ -0,0 +1,34 @@
+package manojromina.videodemo;
+
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView =(VideoView)findViewById(R.id.videoView1);
+
+        //Creating MediaController
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+
+        //specify the location of media file
+        String videopath = "android.resource://manojromina.videodemo/"+R.raw.twinkle;
+        Uri uri= Uri.parse(videopath);
+
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+
+    }
+}
